1e6f913c435f4c81c10d480e77e5505258c207c9
kasperekd <kasperekd@mail.ru>
2025-01-27 15:22:55 +0700

    task_4

--- cmd/day1_main.go
+++ cmd/day1_main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/kasperekd/algorithms/graph"
 )
 
-func main() {
+func task3() {
 	g := graph.NewGraph()
 
 	g.AddEdge(0, 1)
@@ -31,4 +31,42 @@ func main() {
 	fmt.Println("HasEdge(g2, 5, 7):", graph.HasEdge(g2, 5, 7))
 	fmt.Println("HasEdge(g2, 7, 5):", graph.HasEdge(g2, 7, 5))
 	fmt.Println("HasEdge(g2, 5, 8):", graph.HasEdge(g2, 5, 8))
+}
+
+func task4() {
+	g := graph.NewGraph()
+
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+	g.AddEdge(1, 2)
+	g.AddEdge(1, 3)
+	g.AddEdge(2, 4)
+	g.AddEdge(3, 5)
+	g.AddEdge(5, 6)
+	g.AddEdge(6, 4)
+
+	g.PrintGraph()
+
+	bfsOrder := g.BFS(0)
+	fmt.Println("BFS начиная с 0:", bfsOrder)
+
+	bfsOrder2 := g.BFS(2)
+	fmt.Println("BFS начиная с 2:", bfsOrder2)
+
+	bfsOrder3 := g.BFS(5)
+	fmt.Println("BFS начиная с 5:", bfsOrder3)
+
+	g2 := graph.NewGraph()
+	g2.AddEdge(5, 7)
+	bfsOrder4 := g2.BFS(5)
+	fmt.Println("BFS g2 начиная с 5:", bfsOrder4)
+
+	g3 := graph.NewGraph()
+	bfsOrder5 := g3.BFS(0)
+	fmt.Println("BFS обход пустого графа g3 с 0:", bfsOrder5)
+}
+
+func main() {
+	task3()
+	task4()
 }
--- graph/bfs.go
+++ graph/bfs.go
@@ -1,1 +1,30 @@
 package graph
+
+func (g *Graph) BFS(start int) []int {
+	visited := make(map[int]bool)
+	var queue SimpleQueue
+	order := []int{}
+
+	queue.Enqueue(start)
+	visited[start] = true
+
+	for len(queue) > 0 {
+		u, ok := queue.Dequeue()
+		if !ok {
+			break
+		}
+
+		order = append(order, u)
+
+		if neighbors, ok := g.adj[u]; ok {
+			for _, v := range neighbors {
+				if !visited[v] {
+					visited[v] = true
+					queue.Enqueue(v)
+				}
+			}
+		}
+	}
+
+	return order
+}
